1e332de7c7009d79c0ac2fcc33ab342cfb4d566d
Caitlin:git config --global user.name <caitlindonahue95@gmail.com>
2024-03-28 21:46:01 -0500

    add button styling and some infra

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./css");
-
+  eleventyConfig.addPassthroughCopy("./assets");
+  eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   return {
     passthroughFileCopy: true,
   };
