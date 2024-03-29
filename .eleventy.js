f836bcc926388aab96b0e2021f10b929b0cadd3d
Caitlin:git config --global user.name <caitlindonahue95@gmail.com>
2024-03-29 13:55:51 -0500

    file structure changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./css");
-  eleventyConfig.addPassthroughCopy("./assets");
+  eleventyConfig.addPassthroughCopy("./flyght-css");
+  eleventyConfig.addPassthroughCopy("./_assets");
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   return {
