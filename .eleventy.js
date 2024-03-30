114105e220646d7bb0268a53849729a5cc733910
Caitlin:git config --global user.name <caitlindonahue95@gmail.com>
2024-03-29 19:25:39 -0500

    working on build

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
+  const isProd = process.env.ELEVENTY_ENV === "production";
+
   eleventyConfig.addPassthroughCopy("./flyght-css");
-  eleventyConfig.addPassthroughCopy("./_assets");
+  eleventyConfig.addPassthroughCopy("./assets");
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
   return {
     passthroughFileCopy: true,
   };
